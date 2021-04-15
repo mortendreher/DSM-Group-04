7c04ed7f23757629c59a5e0c8892e94e510d4f9e
Lars Andersen <lars-andersen@gmx.de>
2021-04-15 13:17:30 +0200

    Graph implementation + visualization
    first steps with the library networkx. Implementation and  visualization of a graph and its maxflow.

--- group04/src/group04/__init__.py
+++ group04/src/group04/__init__.py
@@ -1,6 +1,19 @@
 __version__ = '0.1.0'
 import maxflow
-import urllib3
-print(maxflow.__version__)
-print(urllib3.__version__)
-print("hello maYHem")
+import networkx as nx
+import matplotlib.pyplot as plt
+G = nx.DiGraph()
+G.add_edge("x", "a", capacity=3.0)
+G.add_edge("x", "b", capacity=1.0)
+G.add_edge("a", "c", capacity=3.0)
+G.add_edge("b", "c", capacity=5.0)
+G.add_edge("b", "d", capacity=4.0)
+G.add_edge("x", "d", capacity=3.0)
+G.add_edge("d", "e", capacity=2.0)
+G.add_edge("c", "y", capacity=2.0)
+G.add_edge("e", "y", capacity=3.0)
+flow_value, flow_dict = nx.maximum_flow(G, "x", "d")
+print(flow_value)
+print(flow_dict)
+nx.draw_networkx(G)
+plt.show()
