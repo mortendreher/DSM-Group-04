dbe4dc67bfb3c7a41d5d60e0dbc095bdf2b124a1
Morten <dreher@mail.hs-ulm.de>
2021-06-11 19:12:20 +0200

    Updated Code for Keno
    Should now account for polyhierarchy

--- group04/src/group04/gift_for_keno.py
+++ group04/src/group04/gift_for_keno.py
@@ -25,18 +25,23 @@ def get_children_ids(conceptid):
 
 def write_graph_to_csv():
     children = get_children_ids(granddad_id)
+    visitednodes = {granddad_id}  # visited nodes saved here (as key without value)
     with open('graph_keno.csv', 'w', newline='') as csvfile:
         graphwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         graphwriter.writerow({granddad_id + get_node_row(granddad_id)})
-        killswitch = 500000  # -> max number of rows
+        killswitch = 500000  # -> max number of rows written before lowest plane
         while len(children) > 0 and killswitch > 0:
             children_todo = []
             for child in children:
-                children_row = get_node_row(child)
-                graphwriter.writerow({child + children_row})
-                killswitch = killswitch - 1
-                if len(children_row) > 0:
-                    children_todo.extend(get_node_row_list(children_row[1:len(children_row)]))  # omit comma
+                if child not in visitednodes:  # child has not been visited yet
+                    children_row = get_node_row(child)
+                    graphwriter.writerow({child + children_row})
+                    visitednodes.update(child)  # set child as visited
+                    killswitch = killswitch - 1
+                    if len(children_row) > 0:
+                        children_todo.extend(get_node_row_list(children_row[1:len(children_row)]))  # omit comma
+                else:
+                    children.remove(child)  # remove child from list of nodes to be visited
             children = children_todo
 
 
